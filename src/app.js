2fc11c82f83e6978ee779c3cc758bfe58a90df88
SatriaAPN <satrianusa10@gmail.com>
2022-03-27 07:04:23 +0700

    initialize the middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// initialize the middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
 // export the REST API endpoint
 app.use(require('./routers'));
 
