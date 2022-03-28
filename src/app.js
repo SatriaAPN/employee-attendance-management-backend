0cb79310f1131f6977868a051589071f6975d66f
SatriaAPN <satrianusa10@gmail.com>
2022-03-28 09:04:07 +0700

    add comment

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded());
 // export the REST API endpoint
 app.use(require('./routers'));
 
+// start the server
 app.listen(3001, () => {
   console.log('the server start listening at port 3001')
 })
